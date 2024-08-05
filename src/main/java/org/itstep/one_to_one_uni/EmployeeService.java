d927b47c835eedb8b445f28e9e4a7616bbabd672
BespalovaAnastasiya123 <3537184@gmail.com>
2024-08-05 20:54:19 +0300

    05/08/2024

---
+++ src/main/java/org/itstep/one_to_one_uni/EmployeeService.java
@@ -0,0 +1,9 @@
+package org.itstep.one_to_one_uni;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+}
